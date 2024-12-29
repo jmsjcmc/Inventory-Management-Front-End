2ecaf5f8dfb686b7e27f0f3d2a4b564a99b18c29
jmsjcmc <jecemecojamestabilog@gmail.com>
2024-12-29 13:47:16 +0800

    Update: Added user dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { UserDashboardComponent } from './user/user-dashboard/user-dashboard.component';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'user-dashboard',
+    component: UserDashboardComponent
   },
   {
     path: '',
